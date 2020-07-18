47231ce7f48b16fd8690318e637a0aab0a564491
Guilherme <guiduck02@gmail.com>
2020-07-17 23:44:27 -0300

    done input for authScreen

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,89 +1,51 @@
 import React from 'react';
 import {
   ScrollView,
+  StyleSheet,
   View,
   KeyboardAvoidingView,
-  StyleSheet,
-  Button
 } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 
 import Input from '../../components/UI/Input';
 import Card from '../../components/UI/Card';
-import Colors from '../../constants/Colors';
 
-const AuthScreen = props => {
-  return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      keyboardVerticalOffset={50}
-      style={styles.screen}
-    >
-      <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
-        <Card style={styles.authContainer}>
-          <ScrollView>
-            <Input
-              id="email"
-              label="E-Mail"
-              keyboardType="email-address"
-              required
-              email
-              autoCapitalize="none"
-              errorMessage="Please enter a valid email address."
-              onInputChange={() => {}}
-              initialValue=""
-            />
-            <Input
-              id="password"
-              label="Password"
-              keyboardType="default"
-              secureTextEntry
-              required
-              minLength={5}
-              autoCapitalize="none"
-              errorMessage="Please enter a valid password."
-              onInputChange={() => {}}
-              initialValue=""
-            />
-            <View style={styles.buttonContainer}>
-              <Button title="Login" color={Colors.primary} onPress={() => {}} />
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button
-                title="Switch to Sign Up"
-                color={Colors.accent}
-                onPress={() => {}}
-              />
-            </View>
-          </ScrollView>
-        </Card>
-      </LinearGradient>
-    </KeyboardAvoidingView>
-  );
+const AuthScreen = (props) => {
+  return;
+  <KeyboardAvoidingView
+    behaviour='padding'
+    keyboardVerticalOffset={50}
+    style={styles.screen}
+  >
+    <Card style={styles.authContainer}>
+      <ScrollView>
+        <Input
+          id='email'
+          label='E-Mail'
+          keyboardType='email.address'
+          required
+          email
+          autoCapitalize='none'
+          errorMessage='Please enter a valid email address!'
+          onValueChange={() => {}}
+          initialValue=''
+        />
+        <Input
+          id='password'
+          label='Password'
+          keyboardType='default'
+          secureTextEntry
+          required
+          minLength={5}
+          autoCapitalize='none'
+          errorMessage='Please enter a valid password!'
+          onValueChange={() => {}}
+          initialValue=''
+        />
+      </ScrollView>
+    </Card>
+  </KeyboardAvoidingView>;
 };
 
-AuthScreen.navigationOptions = {
-  headerTitle: 'Authenticate'
-};
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1
-  },
-  gradient: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  authContainer: {
-    width: '80%',
-    maxWidth: 400,
-    maxHeight: 400,
-    padding: 20
-  },
-  buttonContainer: {
-    marginTop: 10
-  }
-});
+const styles = StyleSheet.create({});
 
 export default AuthScreen;
